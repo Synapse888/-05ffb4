63d4bc8040cb4245d3cda24d3806812399050da1
Synapse888 <egorixios@gmail.com>
2024-10-28 13:17:27 +0200

    ThemeManager.lua

--- ThemeManager.lua
+++ ThemeManager.lua
@@ -3,7 +3,7 @@ local httpService = cloneref(game:GetService('HttpService'))
 local httprequest = (syn and syn.request) or (http and http.request) or http_request or (fluxus and fluxus.request) or request
 local getassetfunc = getcustomasset or getsynasset
 local ThemeManager = {} do
-	ThemeManager.Folder = 'LinoriaLibSettings'
+	ThemeManager.Folder = 'Wave Ware'
 	-- if not isfolder(ThemeManager.Folder) then makefolder(ThemeManager.Folder) end
 
 	ThemeManager.Library = nil
